2e0ae12adc6aa16bd6485280f4cf842e89e1dde1
ufuksahar <sahar@itu.edu.tr>
2017-12-09 23:16:51 +0300

    added insertion operation in scouting page"

--- scouting.py
+++ scouting.py
@@ -19,8 +19,38 @@ def scouting_page():
         cursor = connection.cursor()
 
         if request.method == 'GET':
+            query = """ SELECT x.ID, x.Name, x.Surname, x.Point, x.CreateDate, y.HomeTeamName, y.AwayTeamName, y.ArenaName, y.MatchDate  FROM ObservedPlayerInfo as x JOIN FixtureInfo as y ON x.MatchId = y.Id ORDER BY x.CreateDate DESC"""
+            cursor.execute(query)
+            observedPlayers = cursor.fetchall()
+            connection.commit()
+            return render_template('scouting.html',observedPlayers=observedPlayers)
 
-            return render_template('scouting.html')
+
+@site.route('/scouting/add', methods=['GET', 'POST'])
+@login_required
+def scouting_add():
+    with dbapi2.connect(database.config) as connection:
+        cursor = connection.cursor()
+
+        if request.method == 'GET':
+            query = """ SELECT * FROM FixtureInfo"""
+            cursor.execute(query)
+            match_data = cursor.fetchall()
+            return render_template('scouting_add.html', match_data=match_data)
+
+        else:
+            matchType = request.form['matchType']
+            observedName = request.form['observedName']
+            observedSurname = request.form['observedSurname']
+            observedPoint = request.form['observedPoint']
+            if observedPoint == '':
+                observedPoint = 0
+            query = "INSERT INTO ObservedPlayerInfo(MatchID, ScoutID, Name, Surname, Point,CreateDate) VALUES('%d', '%d', '%s', '%s','%d','%s')" % (int(matchType), current_user.id, observedName, observedSurname, int(observedPoint), datetime.datetime.now())
+            cursor.execute(query)
+
+            connection.commit()
+
+            return redirect(url_for('site.scouting_page'))
 
 
 
