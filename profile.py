23e1236be4b28dc17ab33cc092e15bb2bf951a2a
Taha Çorbacıoğlu <tahacorbacioglu@gmail.com>
2017-12-09 16:57:10 +0300

    profile page is updated.
    admin can select user and update their profile.

--- profile.py
+++ profile.py
@@ -17,7 +17,26 @@ def profile_page():
     if request.method == 'GET':
         with dbapi2.connect(database.config) as connection:
             cursor = connection.cursor()
-            query = "SELECT x.name, x.surname, y.name, x.no, x.birthday FROM userinfo as x JOIN parameters as y on (y.id=x.usertypeid OR y.id=x.cityid OR y.id = x.positionid) WHERE x.userid ='%d'" % int(current_user.id)
+            query = "SELECT usertypeid FROM userinfo WHERE userid='%d'" %int(current_user.id)
+            cursor.execute(query)
+            usertype = cursor.fetchone()
+            if(usertype[0] == 95):
+                query = "SELECT name, surname, userid FROM userinfo"
+                cursor.execute(query)
+                users = cursor.fetchall()
+                return render_template('profile.html', usertype=usertype[0], users=users)
+            else:
+                return render_template('profile.html')
+    else:
+        return redirect(url_for('site.profile_edit_page', userid=int(request.form['User'])))
+
+@site.route('/profile-edit/<int:userid>', methods=['GET', 'POST'])
+@login_required
+def profile_edit_page(userid):
+    if request.method == 'GET':
+        with dbapi2.connect(database.config) as connection:
+            cursor = connection.cursor()
+            query = "SELECT x.name, x.surname, y.name, x.no, x.birthday FROM userinfo as x JOIN parameters as y on (y.id=x.usertypeid OR y.id=x.cityid OR y.id = x.positionid) WHERE x.userid ='%d'" % int(userid)
             cursor.execute(query)
             user = cursor.fetchall()
             query = """ SELECT * FROM PARAMETERS WHERE TYPEID=1"""  # typeid 1 for user type
@@ -29,7 +48,7 @@ def profile_page():
             query = """ SELECT * FROM PARAMETERS WHERE TYPEID=3"""  # typeid 3 for city type
             cursor.execute(query)
             cityTypeData = cursor.fetchall()
-            return render_template('profile.html', user=user, userTypeData=userTypeData, positionTypeData=positionTypeData, cityTypeData=cityTypeData)
+            return render_template('profile_edit.html', id= userid,user=user, userTypeData=userTypeData, positionTypeData=positionTypeData, cityTypeData=cityTypeData)
     else:
-        UserDatabase.updateUser(current_user.id, request.form['Name'], request.form['Surname'], request.form['Type'], request.form['No'], request.form['Birthday'], request.form['Position'], request.form['City'])
+        UserDatabase.updateUser(userid, request.form['Name'], request.form['Surname'], request.form['Type'], request.form['No'], request.form['Birthday'], request.form['Position'], request.form['City'])
         return redirect(url_for('site.profile_page'))
